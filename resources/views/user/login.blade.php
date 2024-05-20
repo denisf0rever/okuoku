b422f045710edb7e2564760fc96bc0e161f156a9
Danil Sherbakov <dandiaborn@gmail.com>
2024-05-20 15:44:55 +0300

    placeholder to label

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,24 +1,26 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-		<title>Авторизация</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-		@include('parts.settings')
-    </head>
-   
-   <body>
-   
-   <div class="popup-form">
+
+<head>
+  <title>Авторизация</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  @include('parts.settings')
+</head>
+
+<body>
+
+  <div class="popup-form">
     <div class="popup-form__wrapper">
       <h2 class="popup-form__title">Войти</h2>
       <span class="popup-form__subtitle">Введите данные для авторизации</span>
       <form action="{{ route('authenticate') }}" method="post" class="popup-form__form">
-		@csrf
-        <input type="text" id="username" name="username" class="popup-form__input" placeholder="Имя пользователя">
+        @csrf
+        <label class="popup-form__label" for="username">Имя пользователя:</label>
+        <input type="text" id="username" name="username" class="popup-form__input">
+        <label class="popup-form__label" for="password">Пароль от личного кабинета:</label>
         <div class="popup-form__input-wrapper">
-          <input type="password" id="password" name="password" class="popup-form__input popup-form__password"
-            placeholder="Пароль от личного кабинета">
+          <input type="password" id="password" name="password" class="popup-form__input popup-form__password">
           <img src="images/eye-password-hide-svgrepo-com.svg" alt="" class="popup-form__password-img">
         </div>
         <input type="submit" class="popup-form__submit popup-form__submit-disabled" disabled value="Далее">
@@ -26,5 +28,6 @@
     </div>
   </div>
 
-  </body>
+</body>
+
 </html>
--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -1,35 +1,42 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-		<title>Регистрация</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-		@include('parts.settings')
-    </head>
-   
-   <body>
-   
-   <div class="popup-form">
+
+<head>
+  <title>Регистрация</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  @include('parts.settings')
+</head>
+
+<body>
+
+  <div class="popup-form">
     <div class="popup-form__wrapper">
       <h2 class="popup-form__title">Регистрация</h2>
       <span class="popup-form__subtitle">Введите данные для регистрации</span>
       <form action="{{ route('register.post') }}" method="post" class="popup-form__form">
-		@csrf
-        <input type="text" id="username" name="username" class="popup-form__input" placeholder="Логин">
-        <input type="text" id="name" name="name" class="popup-form__input" placeholder="Имя">
-        <input type="text" id="last_name" name="last_name" class="popup-form__input" placeholder="Фамилия">
-        <input type="text" id="middle_name" name="middle_name" class="popup-form__input" placeholder="Отчество">
-		<input type="text" id="email" name="email" class="popup-form__input" placeholder="Ваша почта">
+        @csrf
+        <label for="username" class="popup-form__label">Логин:</label>
+        <input type="text" id="username" name="username" class="popup-form__input">
+        <label for="name" class="popup-form__label">Имя:</label>
+        <input type="text" id="name" name="name" class="popup-form__input">
+        <label for="last_name" class="popup-form__label">Фамилия:</label>
+        <input type="text" id="last_name" name="last_name" class="popup-form__input">
+        <label for="middle_name" class="popup-form__label">Отчество:</label>
+        <input type="text" id="middle_name" name="middle_name" class="popup-form__input">
+        <label for="email" class="popup-form__label">Ваша почта:</label>
+        <input type="text" id="email" name="email" class="popup-form__input">
+        <label for="password" class="popup-form__label">Пароль от личного кабинета:</label>
         <div class="popup-form__input-wrapper">
-          <input type="password" id="password" name="password" class="popup-form__input popup-form__password"
-            placeholder="Пароль от личного кабинета">
+          <input type="password" id="password" name="password" class="popup-form__input popup-form__password">
           <img src="images/eye-password-hide-svgrepo-com.svg" alt="" class="popup-form__password-img">
         </div>
-		 
+
         <input type="submit" class="popup-form__submit popup-form__submit-disabled" disabled value="Зарегистрироваться">
       </form>
     </div>
   </div>
 
-  </body>
+</body>
+
 </html>
