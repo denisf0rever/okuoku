c17a7893203e7c6b8ed406148509147a003a12c9
Denis Kadyrgulov <predlozhi@bk.ru>
2024-05-21 09:16:50 +0300

    updated

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -478,34 +478,7 @@
             </li>
           </ul>
         </section>
-        <section class="main__article article">
-          <div class="article__wrapper">
-            <h1 class="article__header">Заголовок статьи</h1>
-            <img src="images/placeholder2.png" alt="" class="article__img">
-            <div class="article__subtitle-wrapper">
-              <span class="article__subtitle">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda
-                doloribus labore quas, aliquam sint provident distinctio explicabo perspiciatis consequuntur nisi at
-                adipisci ullam nam velit facilis nesciunt mollitia aperiam saepe?</span>
-            </div>
-            <span class="article__content-header">Содержание статьи:</span>
-            <ul class="article__content-list">
-              <li class="article__content-item">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda
-                doloribus labore quas, aliquam sint provident distinctio explicabo perspiciatis consequuntur nisi at
-                adipisci ullam nam velit facilis nesciunt mollitia aperiam saepe?</li>
-              <li class="article__content-item">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium
-                delectus adipisci expedita, pariatur, </li>
-            </ul>
-            <div class="article__main-text">
-              <h2 class="article__main-text-header">Подзаголовок</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum error asperiores saepe,
-                voluptatibus facere dignissimos officia praesentium incidunt commodi fugit quidem excepturi alias
-                inventore, quas quo! Maiores rerum quasi excepturi?</p>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum error asperiores saepe,
-                voluptatibus facere dignissimos officia praesentium incidunt commodi fugit quidem excepturi alias
-                inventore, quas quo! Maiores rerum quasi excepturi?</p>
-            </div>
-          </div>
-        </section>
+        
         
 		
 		<!--@include('parts.reviews')-->
