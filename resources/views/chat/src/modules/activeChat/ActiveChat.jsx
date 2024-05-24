bcbaa02cdd174caec1904e7193b0c11f8e16e3d3
Danil Sherbakov <dandiaborn@gmail.com>
2024-05-24 14:14:17 +0300

    menu

--- resources/views/chat/src/modules/activeChat/ActiveChat.jsx
+++ resources/views/chat/src/modules/activeChat/ActiveChat.jsx
@@ -1,15 +1,20 @@
 import ChatHeader from "./components/ChatHeader";
 import ChatBottom from "./components/ChatBottom";
 import ChatList from "./components/ChatList";
+import ChatMenu from "./components/ChatMenu";
+import { useState } from "react";
 
 
 const ActiveChat = (props) => {
 
+  const [isMenuHidden, setIsMenuHidden] = useState(true);
+
   return <div className="operator-chat__active-chat-wrapper active-chat">
     <div className="active-chat__wrapper">
-      <ChatHeader activeChatName={props.activeChat} />
+      <ChatHeader activeChatName={props.activeChat} isMenuHidden={isMenuHidden} setIsMenuHidden={setIsMenuHidden} />
       <ChatList activeChatMessages={props.activeChatMessages} />
       <ChatBottom socket={props.socket} />
+      <ChatMenu isMenuHidden={isMenuHidden} />
     </div>
 
   </div>
