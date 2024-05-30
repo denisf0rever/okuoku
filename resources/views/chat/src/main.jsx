ee939cb2d1bb79979514169ac6ce78c4626c94be
Danil Sherbakov <dandiaborn@gmail.com>
2024-05-30 11:59:09 +0300

    remove chat

--- resources/views/chat/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.scss'
-
-ReactDOM.createRoot(document.getElementById('operator-chat')).render(
-  <App />
-)
