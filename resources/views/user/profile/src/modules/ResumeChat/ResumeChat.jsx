bb8d9e3c47bd8c58d017fc8905afd7d183c846e8
Danil Sherbakov <dandiaborn@gmail.com>
2024-08-04 14:36:23 +0300

    remove choise

--- resources/views/user/profile/src/modules/ResumeChat/ResumeChat.jsx
+++ resources/views/user/profile/src/modules/ResumeChat/ResumeChat.jsx
@@ -29,10 +29,14 @@ const ResumeChat = (props) => {
     props.setIsUserRegistered(true);
   }
 
-  return <div className="user-chat__choices">
-    <div className="user-chat__choice" onClick={() => resumeChat()}>Продолжить чат</div>
-    <div className="user-chat__choice" onClick={() => props.setUserCookie(null)}>Новый чат</div>
-  </div>
+  resumeChat();
+
+  return <></>
+
+  // <div className="user-chat__choices">
+  //   <div className="user-chat__choice" onClick={() => resumeChat()}>Продолжить чат</div>
+  //   <div className="user-chat__choice" onClick={() => props.setUserCookie(null)}>Новый чат</div>
+  // </div>
 }
 
 export default ResumeChat;
