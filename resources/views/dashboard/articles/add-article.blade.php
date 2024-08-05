fc4b64a281317bcbf5d1203b3c2d2d82d33347a7
Denis Kadyrgulov <predlozhi@bk.ru>
2024-08-05 15:08:27 +0300

    05.08

--- resources/views/dashboard/articles/add-article.blade.php
+++ resources/views/dashboard/articles/add-article.blade.php
@@ -30,39 +30,39 @@
                     <div class="form__tab form__tab-active">
                       <ul class="form__inputs">
 						
-                        <li class="form__input-wrapper @error('title')error @enderror">
+                        <li class="form__input-wrapper">
                           <label class="form__label" for="title">Заголовок документа</label>
-                          <input class="form__input" type="text" id="title" name="title">
+                          <input class="form__input @error('title')error @enderror" type="text" id="title" name="title">
                         </li>
-                        <li class="form__input-wrapper @error('h1')error @enderror">
+                        <li class="form__input-wrapper">
                           <label class="form__label" for="title">Заголовок статьи</label>
-                          <input class="form__input" type="text" id="h1" name="h1">
+                          <input class="form__input @error('h1')error @enderror" type="text" id="h1" name="h1">
                         </li>
-                        <li class="form__input-wrapper @error('subtitle')error @enderror">
+                        <li class="form__input-wrapper">
                           <label class="form__label" for="subtitle">Подзаголовок</label>
-                          <input class="form__input" type="text" id="subtitle" name="subtitle">
+                          <input class="form__input @error('subtitle')error @enderror" type="text" id="subtitle" name="subtitle">
                         </li>
-                        <li class="form__input-wrapper @error('metadescription')error @enderror">
+                        <li class="form__input-wrapper">
                           <label class="form__label" for="Meta-description">Метаописание</label>
-                          <input class="form__input" type="text" id="Meta-description" name="metadescription">
+                          <input class="form__input @error('metadescription')error @enderror" type="text" id="Meta-description" name="metadescription">
                         </li>
-                        <li class="form__input-wrapper @error('metakey')error @enderror">
+                        <li class="form__input-wrapper">
                           <label class="form__label" for="Metakeys">Метаключи</label>
-                          <input class="form__input" type="text" id="Metakeys" name="metakey">
+                          <input class="form__input @error('metakey')error @enderror" type="text" id="Metakeys" name="metakey">
                         </li>
-                        <li class="form__input-wrapper @error('author_id')error @enderror">
+                        <li class="form__input-wrapper">
                           <label class="form__label" for="authorID">ID Автора</label>
-                          <input class="form__input" type="text" id="author-id" name="author_id">
+                          <input class="form__input @error('author_id')error @enderror" type="text" id="author-id" name="author_id">
                         </li>
-                        <li class="form__input-wrapper @error('reading-time')error @enderror">
+                        <li class="form__input-wrapper">
                           <label class="form__label" for="reading-time">Время прочтения</label>
-                          <input class="form__input" type="text" id="reading-time" name="reading-time">
+                          <input class="form__input @error('reading-time')error @enderror" type="text" id="reading-time" name="reading-time">
                         </li>
                       </ul>
-                      <label class="form__label-photo @error('image')error @enderror">
+                      <label class="form__label-photo">
                         <img src="images/photo-camera.svg" alt="" class="form__input-photo-img">
                         <span class="form__input-photo-text">Загрузить фото</span>
-                        <input class="form__input-photo" type="file" name="image" accept="image/*">
+                        <input class="form__input-photo @error('image')error @enderror" type="file" name="image" accept="image/*">
                       </label>
                     </div>
                     <div class="form__tab">placeholder</div>
@@ -72,7 +72,7 @@
                       <span class="form__status-title" for="status">Текущий статус</span>
                       <img src="images/expand-more.svg" alt="" class="form__status-arrow">
                       <div class="form__status-current-text">Выберите категорию</div>
-                      <input class="form__status-current" name="category" type="text" value="0" readonly>
+                      <input class="form__status-current @error('category')error @enderror" name="category" type="text" value="0" readonly>
                       <ul id="status" class="form__status-select form__status-hide">
                         <li class="form__status-option" value="1">Категория</li>
                         <li class="form__status-option" value="2">Категория</li>
@@ -80,18 +80,18 @@
                     </div>
                   </div>
                 </div>
-                <div class="form__textarea-wrapper @error('short_text')error @enderror">
+                <div class="form__textarea-wrapper">
                   <div class="form__textarea-title">Краткое описание</div>
-                  <textarea name="short_text" id="" class="form__textarea"></textarea>
+                  <textarea name="short_text @error('short_text')error @enderror" id="" class="form__textarea"></textarea>
                 </div>
-                <div class="form__textarea-wrapper @error('content')error @enderror">
+                <div class="form__textarea-wrapper">
                   <div class="form__textarea-title">Содержаните статьи</div>
-                  <textarea name="content" id="" class="form__textarea"><ul class="article__content-list"><li class="article__content-item"></li></ul></textarea>
+                  <textarea name="content" id="" class="form__textarea @error('content')error @enderror"><ul class="article__content-list"><li class="article__content-item"></li></ul></textarea>
                 </div>
-                <div class="form__textarea-wrapper @error('full_text')error @enderror">
+                <div class="form__textarea-wrapper">
                   <div class="form__textarea-title">Полный текст</div>
                   <textarea name="full_text" id="" class="form__textarea"></textarea>
-                  <input class="form__submit" type="submit"> </input>
+                  <input class="form__submit @error('full_text')error @enderror" type="submit"> </input>
                 </div>
               </form>
             </div>
