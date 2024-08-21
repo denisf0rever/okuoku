4a3ba42f29c26f10738c785b92777e58ce9869bb
Danil Sherbakov <dandiaborn@gmail.com>
2024-08-21 11:18:17 +0300

    grid fix

--- resources/views/articles/item.blade.php
+++ resources/views/articles/item.blade.php
@@ -7,18 +7,18 @@
 @section('content')
 @include('parts.sidebar')
 <section class="main__content-block content-block">
-	<div class="content-block__wrapper">
-		<h1 class="content-block__header">{{ $article->h1 }} Views: {{ $article->views }}</h1>
-		<img src="{{ Storage::url($article->thumb) }}" alt="" class="content-block__img">
-		<div class="content-block__subtitle-wrapper">
-		<span class="content-block__subtitle">{{ $article->subtitle }}</span>
-	</div>
-	<span class="content-block__content-header">Содержание статьи:</span>
-	{{ $article->content }}
-	<div class="content-block__main-text">
-	<h2 class="content-block__main-text-header">Подзаголовок</h2>
-		{{ $article->content }}
-	</div>
-          </div>
+  <div class="content-block__wrapper">
+    <h1 class="content-block__header">{{ $article->h1 }} Views: {{ $article->views }}</h1>
+    <img src="{{ Storage::url($article->thumb) }}" alt="" class="content-block__img">
+    <div class="content-block__subtitle-wrapper">
+      <span class="content-block__subtitle">{{ $article->subtitle }}</span>
+    </div>
+    <span class="content-block__content-header">Содержание статьи:</span>
+    {{ $article->content }}
+    <div class="content-block__main-text">
+      <h2 class="content-block__main-text-header">Подзаголовок</h2>
+      {{ $article->content }}
+    </div>
+  </div>
 </section>
 @endsection
--- resources/views/parts/sidebar__mainpage.blade.php
+++ resources/views/parts/sidebar__mainpage.blade.php
@@ -1,62 +1,62 @@
   <div class="main__wrapper">
-      <div class="main__container container">
-	  <nav class="main__navigation navigation">
-          <ul class="navigation__list">
-            <li class="navigation__item">
-              <a href="" class="navigation__link">
-                <img src="images/discord.svg" alt="" class="navigation__image">
-                <div class="navigation__notification"></div>
-                <span class="navigation__title">Главная</span>
-              </a>
-            </li>
-            <li class="navigation__item">
-              <a href="" class="navigation__link">
-                <img src="images/discord.svg" alt="" class="navigation__image">
-                <div class="navigation__notification"></div>
-                <span class="navigation__title">Подписки</span>
-              </a>
-            </li>
-            <li class="navigation__item">
-              <a href="" class="navigation__link navigation__link_selected">
-                <img src=" images/discord.svg" alt="" class="navigation__image">
-                <div class="navigation__notification"></div>
-                <span class="navigation__title">Темы</span>
-              </a>
-            </li>
-            <li class="navigation__item">
-              <a href="" class="navigation__link">
-                <img src="images/discord.svg" alt="" class="navigation__image">
-                <div class="navigation__notification"></div>
-                <span class="navigation__title">Видео</span>
-              </a>
-            </li>
-            <li class="navigation__item">
-              <a href="" class="navigation__link">
-                <img src="images/discord.svg" alt="" class="navigation__image">
-                <div class="navigation__notification"></div>
-                <span class="navigation__title">Статьи</span>
-              </a>
-            </li>
-            <li class="navigation__item">
-              <a href="" class="navigation__link">
-                <img src="images/discord.svg" alt="" class="navigation__image">
-                <div class="navigation__notification"></div>
-                <span class="navigation__title">Ролики</span>
-              </a>
-            </li>
-            <li class="navigation__item">
-              <a href="" class="navigation__link">
-                <img src="images/discord.svg" alt="" class="navigation__image">
-                <div class="navigation__notification"></div>
-                <span class="navigation__title">Новости</span>
-              </a>
-            </li>
-            <li class="navigation__item">
-              <a href="" class="navigation__link">
-                <img src="images/discord.svg" alt="" class="navigation__image">
-                <div class="navigation__notification"></div>
-                <span class="navigation__title">Сохранённое</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
+    <div class="main__container-main container">
+      <nav class="main__navigation navigation">
+        <ul class="navigation__list">
+          <li class="navigation__item">
+            <a href="" class="navigation__link">
+              <img src="images/discord.svg" alt="" class="navigation__image">
+              <div class="navigation__notification"></div>
+              <span class="navigation__title">Главная</span>
+            </a>
+          </li>
+          <li class="navigation__item">
+            <a href="" class="navigation__link">
+              <img src="images/discord.svg" alt="" class="navigation__image">
+              <div class="navigation__notification"></div>
+              <span class="navigation__title">Подписки</span>
+            </a>
+          </li>
+          <li class="navigation__item">
+            <a href="" class="navigation__link navigation__link_selected">
+              <img src=" images/discord.svg" alt="" class="navigation__image">
+              <div class="navigation__notification"></div>
+              <span class="navigation__title">Темы</span>
+            </a>
+          </li>
+          <li class="navigation__item">
+            <a href="" class="navigation__link">
+              <img src="images/discord.svg" alt="" class="navigation__image">
+              <div class="navigation__notification"></div>
+              <span class="navigation__title">Видео</span>
+            </a>
+          </li>
+          <li class="navigation__item">
+            <a href="" class="navigation__link">
+              <img src="images/discord.svg" alt="" class="navigation__image">
+              <div class="navigation__notification"></div>
+              <span class="navigation__title">Статьи</span>
+            </a>
+          </li>
+          <li class="navigation__item">
+            <a href="" class="navigation__link">
+              <img src="images/discord.svg" alt="" class="navigation__image">
+              <div class="navigation__notification"></div>
+              <span class="navigation__title">Ролики</span>
+            </a>
+          </li>
+          <li class="navigation__item">
+            <a href="" class="navigation__link">
+              <img src="images/discord.svg" alt="" class="navigation__image">
+              <div class="navigation__notification"></div>
+              <span class="navigation__title">Новости</span>
+            </a>
+          </li>
+          <li class="navigation__item">
+            <a href="" class="navigation__link">
+              <img src="images/discord.svg" alt="" class="navigation__image">
+              <div class="navigation__notification"></div>
+              <span class="navigation__title">Сохранённое</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
