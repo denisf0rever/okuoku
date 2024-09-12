04f84db16de4219a429c1483269fd968fa79f3a9
Danil Sherbakov <dandiaborn@gmail.com>
2024-09-12 12:43:44 +0300

    sidebar__fullscreen

--- resources/views/consult/form.blade.php
+++ resources/views/consult/form.blade.php
@@ -5,7 +5,7 @@
 @section('canonical', 'consultation/create')
 
 @section('content')
-@include('parts.sidebar')
+@include('parts.sidebar__fullscreen')
 <section class="main__content-block content-block">
   <div class="content-block__wrapper">
     <h1 class="content-block__header"> </h1>
